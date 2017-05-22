6000d8de9f4f1129dc3fb0c9e1282b1dbd3c9c03
Lee Morris <leebenmorris@gmail.com>
2017-05-22 23:36:50 +0100

    added tests for s3EventHandler function

--- backend/spec/process-xml.spec.js
+++ backend/spec/process-xml.spec.js
@@ -2,6 +2,8 @@ const { expect } = require('chai');
 
 const { handler, s3EventHandler } = require('../lambda-functions/process-xml');
 
+const { s3Event } = require('./s3-sample-event');
+
 describe('handler', () => {
   it('is a function', () => {
     expect(handler).to.be.a('function');
@@ -11,5 +13,30 @@ describe('handler', () => {
 describe('s3EventHandler', () => {
   it('is a function', () => {
     expect(s3EventHandler).to.be.a('function');
+  });
+
+  it('returns an object with correct srcKey and srcBucket when given an s3 event', () => {
+    const actual = s3EventHandler(s3Event);
+    const expected = {
+      srcBucket: 'first-xml',
+      srcKey: 'example-1.xml.txt'
+    };
+    expect(actual).to.eql(expected);
+  });
+
+  it('returns false when NOT given an s3 event', () => {
+    s3Event.Records[0].eventSource = 'x';
+
+    let actual = s3EventHandler(s3Event);
+    expect(actual).to.be.false;
+
+    actual = s3EventHandler({});
+    expect(actual).to.be.false;
+
+    actual = s3EventHandler(1);
+    expect(actual).to.be.false;
+
+    actual = s3EventHandler('a');
+    expect(actual).to.be.false;
   });
 });
