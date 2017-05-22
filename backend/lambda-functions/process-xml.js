3e4ceefab94fc6aecf5bfec96f93ab453823148f
Lee Morris <leebenmorris@gmail.com>
2017-05-22 23:38:15 +0100

    s3EventHandler complete, added console.logs to handler for CloudWatch logs

--- backend/lambda-functions/process-xml.js
+++ backend/lambda-functions/process-xml.js
@@ -1,10 +1,18 @@
 const s3EventHandler = event => {
-  console.log('Reading event:\n', JSON.stringify(event, null, 2));
+  const s3event = event.Records && event.Records[0];
+  return (s3event && s3event.eventSource === 'aws:s3' && s3event.s3)
+    ? {
+      srcBucket: s3event.s3.bucket.name,
+      srcKey: s3event.s3.object.key
+    }
+    : false;
 };
 
 const handler = (event, context) => {
   context.callbackWaitsForEmptyEventLoop = false;
-  s3EventHandler(event);
+  const s3Result = s3EventHandler(event);
+  console.log('s3 Event Result:\n', JSON.stringify(s3Result, null, 2));
+  console.log('s3 Event Result Statement: ', s3Result && 's3 event handled');
 };
 
 module.exports = {
