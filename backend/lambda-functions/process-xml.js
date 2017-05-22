f42af61dacb0a8b55e952a5c75beef277375fb51
Lee Morris <leebenmorris@gmail.com>
2017-05-22 18:14:15 +0100

    simplified function, removed try/ catch as no async at this stage

--- backend/lambda-functions/process-xml.js
+++ backend/lambda-functions/process-xml.js
@@ -1,14 +1,4 @@
-async function processXml(event) {
-  try {
-    console.log('Reading event:\n', JSON.stringify(event, null, 2));
-    return 'Process completed successfully';
-  }
-  catch (err) { return err; }
-}
-
 exports.handler = (event, context) => {
   context.callbackWaitsForEmptyEventLoop = false;
-  processXml(event)
-    .then(res => console.log(res))
-    .catch(err => console.log(err));
+  console.log('Reading event:\n', JSON.stringify(event, null, 2));
 };
