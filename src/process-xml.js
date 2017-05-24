a7ca8f700a920127e6c4724879a0f30af184c8a8
Lee Morris <leebenmorris@gmail.com>
2017-05-24 11:44:15 +0100

    lambda now converting buffer to JSON

--- src/process-xml.js
+++ src/process-xml.js
@@ -1,22 +1,19 @@
 const h = require('../helpers/helpers');
 
-const AWS = require('aws-sdk');
-
-const s3 = new AWS.S3();
-
 exports.handler = async (event, context) => {
   context.callbackWaitsForEmptyEventLoop = false;
   try {
-    console.log('Reading event:\n', JSON.stringify(event, null ,2));
     const { srcBucket, srcKey } = h.s3EventHandler(event);
     const dstBucket = srcBucket + '-archived';
     const dstKey = 'archived.' + srcKey;
 
     console.log('names:\n', srcBucket, srcKey, dstBucket, dstKey);
 
-    const srcBuffer = (await h.download(srcBucket, srcKey, s3)).Body;
+    const srcBuffer = (await h.download(srcBucket, srcKey)).Body;
 
-    console.log(srcBuffer);
+    const json = await h.bufferToJson(srcBuffer);
+
+    console.log('\n', JSON.stringify(json, null, 2));
   }
   catch (err) { console.log(err); }
 };
