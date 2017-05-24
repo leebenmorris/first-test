77e808b48717c25770bd513255dd58272f5e34c2
Lee Morris <leebenmorris@gmail.com>
2017-05-24 20:19:59 +0100

    config to webpack test environment for current version of node

---
+++ local.webpack.config.js
@@ -0,0 +1,40 @@
+const nodeExternals = require('webpack-node-externals');
+const path = require('path');
+
+module.exports = {
+  entry: {
+    test: './spec/process-xml.spec',
+    'init-db': './db-config/init-db'
+  },
+  target: 'node',
+  externals: [nodeExternals()],
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name]-bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: [
+            [
+              'env',
+              {
+                targets: {
+                  // targets the version of node running the build script
+                  node: 'current'
+                }
+              }
+            ]
+          ],
+          plugins: [
+            'transform-runtime'
+          ]
+        }
+      }
+    ]
+  }
+};
